ba9373cf2f7d9e6c2c3c4ec1ca74b2eb3f9f8b35
Axel Karlsson <axeka379@student.liu.se>
2020-12-17 10:37:46 +0100

    Pushing todo_list

---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+import {List} from "@material-ui/core"
+
+import React from "react"
+import Todo from "./Todo"
+//const { default: TodoList } = require("./TodoList")
+
+function TodoList({todos, toggleComplete, removeTodo}){
+    return (
+        <List>
+            {todos.map(todo => (
+               <Todo 
+                key={todo.id} 
+                todo={todo}
+                toggleComplete={toggleComplete}
+                removeTodo={removeTodo}
+                /> 
+            ))}
+        </List>
+    )
+}
+
+export default TodoList
